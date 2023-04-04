1d08fca72025c33d343d3743bc1507c8e80580cd
Вениамин Козицкий <destin321@mail.ru>
2023-04-05 00:08:09 +0400

    Second

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 ﻿#include <iostream> 
 using namespace std;
+
+int f(int n)
+{
+    if (n == 2 || n == 3) return (n - 1);
+    return f(n - 1) + f(n - 2);
+}
+
 int main()
 {
-    cout << "Hello, world!";
+    int n;
+    cout << "n="; cin >> n;
+    cout << f(n) << " ";
+    cout << endl;
+    system("pause");
     return 0;
 }
